f6ff57d10d042a2634cf5387c2dff720448ad7be
wqofjqnweokfg <163450576+wqofjqnweokfg@users.noreply.github.com>
2024-03-14 17:58:29 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('gcubesBtn').addEventListener('click', function() {
+    // Replace 'VIDEO_ID' with the actual ID of the YouTube video you want to redirect to
+    window.location.href = 'https://grabify.link/0XQKWP';
+});
